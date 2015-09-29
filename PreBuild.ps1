3fe65837b66adc91efc66701a8b04f8ae96cc33a
Thiago Almeida <nzthiago@live.com>
2015-09-29 07:46:27 +0000

    Trying a fix for path too long

--- PreBuild.ps1
+++ PreBuild.ps1
@@ -20,5 +20,7 @@ else
 # ex: & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -r coreclr
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
+new-psdrive -Name X -PSProvider FileSystem -Root $PSScriptRoot\src
+
  # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
+Get-ChildItem -Path "x:\" -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
